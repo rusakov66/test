e709ad0d7e065953f7e6382f4242e14889702415
rusakov66 <rusakov66@gmail.com>
2018-11-30 00:54:59 +0300

    345

--- src/rusakov/ResolvedClass.java
+++ src/rusakov/ResolvedClass.java
@@ -1,4 +1,22 @@
 package rusakov;
-
+import java.util.Scanner;
 public class ResolvedClass {
+    public static <string> void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input string: ");
+        String str = in.nextLine();
+        boolean flag = true;
+        /* delaem cikl ot 0 do poloviny dliny stroki */
+        for (int i = 0; i <= (str.length() / 2) - 1; i = i + 1) {
+            /* Vivodim sravnimuiu paru chisel */
+            System.out.print(str.charAt(i));
+            System.out.print("--");
+            System.out.println(str.charAt(str.length() - 1 - i));
+            if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+                flag = flag;
+            } else {flag = flag & false;}
+        }
+        if (flag) {System.out.println("Polindrom");} else {System.out.println("Non polindrom");}
+    }
 }
+/*   branch PROBLEM_1 */
